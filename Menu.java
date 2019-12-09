b76fba56d5a3466efbd2e3994e8fc976ae104841
Junior Fernandes <juniorbass8@gmail.com>
2019-12-09 11:59:23 -0300

    Made some adjustments

--- Menu.java
+++ Menu.java
@@ -4,70 +4,68 @@ import java.util.Scanner;
 
 public class Menu {
 	//method declared as static to be used in all methods
-	static Scanner leia = new Scanner(System.in);
+	static Scanner read = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// declaration of variable
-		int opcao = 0;
-		int digit;
-        int calculated;
-        String ean;
-        String checkSum = "131313131313";
-        int sum = 0;
-		String codeBar, codigodeBarra;
+		int calculated, digit, option = 0, sum = 0; //
+        String ean, codeBar, checkCountry, checkSum = "131313131313";
 		
 		// loop where will stop when the value of the variable equals zero
 		do {
 			cleanScreen();
-			System.out.println("-------------------------------");
+			System.out.println("--------------------------------------");
 			System.out.println("           VAREJO S/A ");
 			System.out.println("    Código GTIN-13/upc/ean-13 ");
-			System.out.println("-------------------------------");
+			System.out.println("--------------------------------------");
 			System.out.println("  1)Validar código GTIN-13 ");
 			System.out.println("  2)Identificar País       ");
 			System.out.println("  0)Sair                   ");
-			System.out.println("-------------------------------");
+			System.out.println("--------------------------------------");
 			
 			System.out.print("  Digite uma opção: ");
-			opcao = leia.nextInt();
+			option = read.nextInt();
 
 			System.out.print("\n");
 			
-			switch (opcao) {
+			switch (option) {
 			case 1:
 				cleanScreen();
-				System.out.println("-------------------------------");
+				System.out.println("--------------------------------------");
 				System.out.println("           VAREJO S/A ");
 				System.out.println("    Código GTIN-13/upc/ean-13 ");
-				System.out.println("-------------------------------");
+				System.out.println("--------------------------------------");
 				System.out.println("  VALIDAR CÓDIGO ");
 				
 				System.out.print("  Digite o código de 13 digitos: ");
-				codeBar = leia.next();
-				System.out.println("-------------------------------");
+				codeBar = read.next();
+				System.out.println("--------------------------------------");
 				
 				if(codeBar.length() == 13) {
-					digit = Integer.parseInt("" + codeBar.charAt(codeBar.length() - 1));            
-		            ean = codeBar.substring(0, codeBar.length() - 1);            
+					//
+					digit = Integer.parseInt("" + codeBar.charAt(codeBar.length() - 1));     
+					
+					//       
+		            ean = codeBar.substring(0, codeBar.length() - 1);
+					
+					//
 		            for (int i = 0; i <= ean.length() - 1; i++) {
 		                sum += (Integer.parseInt("" + ean.charAt(i))) * (Integer.parseInt("" + checkSum.charAt(i)));
-		            }            
+		            }
+
 		            calculated = 10 - (sum % 10);
 		            
+					//
 		            if(calculated == digit) {
 		            	System.out.println("  DIGITO VERIFICADOR: " + digit);
 		            	System.out.println("  NÚMERO GTIN-13 VÁLIDO!");
 		            	sum = 0;
-		            	System.out.println("--------------------------------------");
-						System.out.println("  Precione ENTER para continuar...");
-						leia.nextLine();leia.nextLine();
+		            	nextStep();
 		            }else {
 		            	System.out.println("  DIGITO VERIFICADOR: " + calculated);
 		            	System.out.println("  NÚMERO GTIN-13 INVÁLIDO!");
 		            	sum = 0;
-		            	System.out.println("--------------------------------------");
-						System.out.println("  Precione ENTER para continuar...");
-						leia.nextLine();leia.nextLine();
+		            	nextStep();
 		            }
 				}else {
 					System.out.println("  Número GTIN-13 não possui 13 dígitos!");
@@ -83,17 +81,16 @@ public class Menu {
 				System.out.println("  IDENTIFICAR CÓDIGO ");
 				
 				System.out.print("  Digite o código de 13 digitos: ");
-				codeBar = leia.next();
+				codeBar = read.next();
 				
 				if(codeBar.length() == 13) {
-					codigodeBarra = codeBar.substring(0,3);
+					//assignment of the first 3 digits of the value contained in the codeBar variable
+					checkCountry = codeBar.substring(0,3);
 					
-					switch (codigodeBarra) {
+					switch (checkCountry) {
 					case "789":
 						System.out.println("  GTIN-13 origem Brasil");
-						System.out.println("--------------------------------------");
-						System.out.println("  Precione ENTER para continuar...");
-						leia.nextLine();leia.nextLine();
+						nextStep();
 						break;
 					case "790":
 						System.out.println("  GTIN-13 origem Brasil");
@@ -136,24 +133,22 @@ public class Menu {
 						nextStep();
 						break;
 					default:
-						System.out.println("VAREJOS S/A não vende para este país: " + codigodeBarra);
+						System.out.println("VAREJOS S/A não vende para este país: " + checkCountry);
 						nextStep();
 					}
 				}else {
 					System.out.println("  Número GTIN-13 não possui 13 dígitos!");
 					nextStep();
 				}
-				
-				codigodeBarra = codeBar.substring(0,3);
-				
 				break;
 			case 0:
 				break;
 			default:
-				System.out.println("Opção Inválida!");
+				System.out.println("  Opção Inválida!");
+				nextStep();
 				break;
 			}
-		} while (opcao != 0);
+		} while (option != 0);
 	}
 	
 	// method to clean the screen
@@ -165,8 +160,6 @@ public class Menu {
 	public static void nextStep() {
 		System.out.println("--------------------------------------");
 		System.out.println("  Precione ENTER para continuar...");
-		leia.nextLine();leia.nextLine();
+		read.nextLine();read.nextLine();
 	}
-
 }
-
